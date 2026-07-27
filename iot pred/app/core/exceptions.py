89e43cb0880104ed1e359aa5e2dc8a337109227d
Gokul Kavin <gokulkavin000003@gmail.com>
2026-07-27 17:41:48 +0530

    feat(core): add custom exceptions

--- iot pred/app/core/exceptions.py
+++ iot pred/app/core/exceptions.py
@@ -1,2 +1,27 @@
-# TODO: Custom application exceptions
-# Classes: ApplicationError, NotFoundError, UnauthorizedError, ValidationError, DatabaseError
+"""Custom exception classes for the application."""
+
+from fastapi import HTTPException
+
+
+class ResourceNotFoundException(HTTPException):
+    """Exception raised when a resource is not found."""
+    def __init__(self, message: str = "Resource not found"):
+        super().__init__(status_code=404, detail=message)
+
+
+class UnauthorizedException(HTTPException):
+    """Exception raised when user is not authorized."""
+    def __init__(self, message: str = "Unauthorized"):
+        super().__init__(status_code=401, detail=message)
+
+
+class BadRequestException(HTTPException):
+    """Exception raised for bad/invalid requests."""
+    def __init__(self, message: str = "Bad request"):
+        super().__init__(status_code=400, detail=message)
+
+
+class ConflictException(HTTPException):
+    """Exception raised when there is a conflict."""
+    def __init__(self, message: str = "Conflict"):
+        super().__init__(status_code=409, detail=message)
